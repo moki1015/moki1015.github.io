840039ffb34f4331c91745ed939c67edcc5a7b36
moki1015 <126322866+moki1015@users.noreply.github.com>
2023-02-25 19:44:06 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const FormAutoFill = new Vue({
   data: {
 
     // Google Apps Script 部署為網路應用程式後的 URL
-    gas: 'https://script.google.com/macros/s/AKfycbwyY7oLpoZ4lbcIwIWniLZ8U_pY8e7YBK9fakeszgR_77S2u1SV/exec',
+    gas: 'https://script.google.com/home/projects/16Cs0XfWYxBX9ok7SkWV9c6j8A6glx5Lnc-O2Hmh7Hi6IGnG89QZd94SR/edit',
 
     id: '',
 
@@ -14,16 +14,13 @@ const FormAutoFill = new Vue({
     person: {},
 
     // Google Form 的 action URL
-    formAction: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeNBnd-yVJ7_-tMq5xaQrvt0j18UtabCFBTM0Eu2O3ivDecuQ/formResponse',
+    formAction: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSd6e66_HLGWQAcKWIFFRyrGDDSsFSSFFfG_HhLlv8TT3XI-ag/formResponse',
     
     // Google Form 各個 input 的 name
     input: {
-      id: 'entry.1815052017',
-      name: 'entry.1543576845',
-      gender: 'entry.892230025',
-      phone: 'entry.1003128242',
-      site: 'entry.260985931',
-      msg: 'entry.1782841550'
+      id: 'entry.215550374',
+      name: 'entry.314711706',
+      phone: 'entry.1205246287',
     },
 
     // loading 效果要不要顯示
@@ -32,8 +29,8 @@ const FormAutoFill = new Vue({
   methods: {
     // ID 限填 4 碼
     limitIdLen(val) {
-      if(val.length > 4) {
-        return this.id =  this.id.slice(0, 4);
+      if(val.length > 10) {
+        return this.id =  this.id.slice(0, 10);
       }
     },
     // 送出表單
@@ -54,7 +51,7 @@ const FormAutoFill = new Vue({
   watch: {
     id: function(val) {
       // ID 輸入到 4 碼就查詢資料
-      if(val.length === 4) {
+      if(val.length === 10) {
 
         // this.persons 裡沒這筆資料，才 POST
         if(this.persons[this.id] === undefined) {
